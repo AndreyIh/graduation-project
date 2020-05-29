cfb7ee9b1baaf43808a3c87d6d2dbf809c4e2fb2
Andrey <Andreyihnatchenko@gmail.com>
2020-05-29 18:51:44 +0300

    debug

--- base/settings/production.py
+++ base/settings/production.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['db-blog-20.herokuapp.com']
 
