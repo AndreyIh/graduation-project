cdedf95ae5afad812cc6426c14ba0d663df805a3
Andrey <Andreyihnatchenko@gmail.com>
2020-06-04 08:19:09 +0300

    debug 2 for heroku

--- base/asgi.py
+++ base/asgi.py
@@ -13,6 +13,8 @@ import os
 import django
 from channels.routing import get_default_application
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings")
 django.setup()
 application = get_default_application()
+
--- base/settings/production.py
+++ base/settings/production.py
@@ -235,5 +235,6 @@ CHANNEL_LAYERS = {
         'CONFIG': {
             "hosts": [(os.environ.get('REDIS_URL'), 6379)],
         },
+        "ROUTING": "chat.routing.channel_routing",
     },
 }
