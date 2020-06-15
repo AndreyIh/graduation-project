768302e3fe2be65db0c24dc448b9a9cd5c1d0009
Andrey <Andreyihnatchenko@gmail.com>
2020-06-15 14:47:50 +0300

    debug

--- private_chat/routing.py
+++ private_chat/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/dialogs/(?P<room_name>\d+-\d+)/$', consumers.ChatConsumer),
+    re_path(r'wss/dialogs/(?P<room_name>\d+-\d+)/$', consumers.ChatConsumer),
 ]
